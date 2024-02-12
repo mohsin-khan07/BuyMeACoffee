61c3ddb48f6cf6e069874c5565ac606644df50b7
Mohsin Khan <mkhan6720@gmail.com>
2024-02-12 23:17:13 +0530

    contract completed

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -2,5 +2,41 @@
 pragma solidity ^0.8.9;
 
 contract BuyMeACoffee {
-    
+    address payable owner;
+
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    Memo[] memos;
+
+    event NewMemo(address indexed from, uint256 timestamp, string name, string message);
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function buyCoffee(string memory _name, string memory _message) public payable {
+        require(msg.value > 0, "Can't buy a coffee with 0 eth");
+
+        memos.push(Memo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        ));
+
+        emit NewMemo(msg.sender, block.timestamp, _name, _message);
+    }
+
+    function withdrawTips() public {
+        require(owner.send(address(this).balance));
+    }
+
+    function getMemos() public view returns(Memo[] memory) {
+        return memos;
+    }
 }
